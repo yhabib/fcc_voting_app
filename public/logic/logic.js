310e9f02e8055ac99e84b7960b073fd6a6677a75
Yusef Habib <habibfernandez@gmail.com>
2016-12-13 20:46:23 +0100

    Materialize and jQuery for the front End

---
+++ public/logic/logic.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("initialized");
+    
+    // Matirialize initializations!!
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
+});
