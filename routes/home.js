ec947ee4359588247cf6ee5c047b1eb6f72fdf77
Yusef Habib <habibfernandez@gmail.com>
2016-12-13 20:47:38 +0100

    No need to have a reference to the db inside the req, model takes care of

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,27 @@
-var express = require('express'),
+let express = require('express'),
+    interface = require('./../model.interface'),
     router = express.Router();
 
+// Get all the polls and render them into the page
 
 router
     .route('/')
-    .get((req, res) => res.render('index', {hostname: req.headers.host}));
+    .get((req, res) => {
+        let obj = {
+                name: "Peter",
+                creator: "Ad",
+                options: [ "a", "b"]
+            };
+
+        interface.insertPoll(obj, (err) => {
+            if(err) throw err;
+            else console.log("inserted");
+        });        
+        interface.getPolls({}, (err, docs) => {
+            if(err) throw err;
+            else 
+                res.render('index', {polls: docs});
+        });
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 const MONGOUSERPSW = process.env.MONGO_USER_PSW;
 
+
 /********************************************************************************
     -- PACKAGES --
 ********************************************************************************/
+
 let express = require('express'),
     pug = require('pug'),
     path = require('path'),
     mongodb = require('mongodb'),
     mongoose = require('mongoose'),
     morgan = require('morgan');
-    
+
+
 /********************************************************************************
     -- ROUTES --
 ********************************************************************************/
+
 let home = require('./routes/home');
+
+
 /********************************************************************************
     -- INIT --
 ********************************************************************************/
+
 let mongo = mongodb.MongoClient,
     url = `mongodb://${MONGOUSERPSW}@ds129018.mlab.com:29018/fcc_voting_app`,
-    app = express();          
+    schema = require('./schema'),
+    app = express();     
+
+url = "mongodb://localhost:27017/fcc_voting_app";
+
 
 /********************************************************************************
     -- APP --
 ********************************************************************************/
-
-
-mongo.connect(url, (err, db) => {
+mongoose.Promise = global.Promise;
+mongoose.connect(url, (err, db) => {
     if(err) 
         throw err;
     else {
         console.log( 'Success: Connected to DB' );
-        app.use((req, res, next) => {
-            req.db = db;
-            next();
-        })
         app.use('/', home);
     }
 });
