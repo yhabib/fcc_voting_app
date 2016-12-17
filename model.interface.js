364a14fba43df5bab6775594832e7dec03656f7d
Yusef Habib <habibfernandez@gmail.com>
2016-12-17 16:48:20 +0100

    Voting logik

--- model.interface.js
+++ model.interface.js
@@ -3,17 +3,17 @@ let mongoose = require('mongoose'),
     Model = mongoose.model('Poll', schema, 'polls');
 
 
+exports.getPollById = function(query, callback) {   
+    Model.findOne(query, callback);
+};
+
 exports.getPolls = function(filter, projection, callback) {    
     Model.find(filter, projection, callback);
 };
 
-exports.getPollById = function(filter, callback) {   
-    Model.findOne(filter, callback);
-};
-
 // Cheks:
     // Does the poll already exist?
-    // 
+    // How can you check that? Exactly name && options && user??
 exports.insertPoll = function(obj, callback) {
     let Poll = new Model({
             name: obj.name,
@@ -23,4 +23,23 @@ exports.insertPoll = function(obj, callback) {
         });
     
     Poll.save(callback);
+};
+
+exports.findOneAndUpdate = function(id, voter, value, callback) {
+    const query = {
+            _id: id,
+            voters: { $nin: [voter] },
+            "options.value": value
+        },
+        update = { 
+            $push: { voters: voter },
+            $inc: { "options.$.count": 1 }
+        },
+        options = { new: true };
+
+        console.log(query);
+        
+        console.log(options);
+    
+    Model.findOneAndUpdate(query, update, options, callback);
 };
--- routes/poll.js
+++ routes/poll.js
@@ -2,16 +2,26 @@ let express = require('express'),
     interface = require('./../model.interface'),
     router = express.Router();
 
+// Check if the user already voted,
+// if not increase it and then
+
 router
     .route('/:id')
-    .get((req, res) => {
-        let filter = {
-                _id: req.params.id
-            };
+    .post((req, res) => {
+        const id = req.params.id,
+              user = "rey",
+              value = req.body.value;
 
-        interface.getPollById(filter, (err, doc) => {
-            if(err) throw err;            
-            res.render('poll', {poll: doc, user: {name: "Yusef"}});
+        // If the user already voted -> nothign to do here
+        interface.findOneAndUpdate(id, user, value, (err, doc) => {
+            if(err) throw err;
+            console.log(doc);
+            if(doc)
+                res.render('poll', {poll: doc, user: {name: "Yusef"}});
+            else {
+                // interface.countVote(filter);
+                res.end("Nothing")
+            }
         });
     });
 
