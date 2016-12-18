9b0d9646e8c63147933c16c94881045d77c8bb32
Yusef Habib <habibfernandez@gmail.com>
2016-12-18 10:29:33 +0100

    Charts logic goes into the corresponding .pug

--- public/logic/logic.js
+++ public/logic/logic.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Materialize initializations!!
     $(".dropdown-button").dropdown();
     $(".button-collapse").sideNav();
@@ -9,12 +9,12 @@ $(document).ready(function() {
         box_count = 2,
         letters = ["c", "d", "e", "f", "g", "h", "i", "j", "k"],
         wrapper = $(".input_wrap"),
-        add_button = $("#add");            
+        add_button = $("#add");
 
-    add_button.click(function(e) {
+    add_button.click(function (e) {
         e.preventDefault();
-        
-        if(box_count >= max_fields) return;
+
+        if (box_count >= max_fields) return;
         let letter = letters[box_count - 2].toUpperCase(),
             template = '<div class="row"><div class="input-field col s8 offset-s2 m5 offset-m5"><input class="validate" id="option' + letter + '" type="text" name="Option' + letter + '" required"><label for="option' + letter + '">Option ' + letter + '</label></div><a id="remove" class="btn-floating btn-small waves-effect waves-light grey font"><i class="material-icons">close</i></a></div>';
         box_count++;
@@ -23,12 +23,9 @@ $(document).ready(function() {
     });
 
     // Special syntax why??
-    wrapper.on("click", "#remove", function(e) {
-        e.preventDefault(); 
+    wrapper.on("click", "#remove", function (e) {
+        e.preventDefault();
         $(this).parent('div').remove()
         box_count--;
     });
-
-    // Voting 
-
 });
